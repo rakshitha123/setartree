a0d0aed415bd0f2928d119e375aeb0ae427f2a67
rakshithawg <rakshithagw@gmail.com>
2022-12-23 00:01:34 +1100

    Updated demos

--- demo/forest_demo.R
+++ demo/forest_demo.R
@@ -1,12 +1,17 @@
+if (!('forecast' %in% rownames(installed.packages())))
+  install.packages("forecast")
+
 library(setartree)
+library(forecast) # The 'forecast' package is only required for plotting in line 17 and 18.
+
 
 ### Case 1 - When need to obtain forecasts for a list of time series ###
 
 # Training SETAR-Forest
-forest1 <- setarforest(train_series, bagging_freq = 3)
+forest1 <- setarforest(chaotic_logistic_series, bagging_freq = 3)
 
 # Obtaining predictions
-forest_forecasts <- forecast(forest1, train_series)
+forest_forecasts <- setartree:::forecast(forest1, chaotic_logistic_series)
 
 # Plotting
 plot(forest_forecasts[[1]]) # Plotting the 1st time series
@@ -15,16 +20,16 @@ plot(forest_forecasts[[10]]) # Plotting the 10th time series
 
 
 
-### Case 2 - When need to obtain predictions for a dataframe/matrix containing model inputs including past time series lags, numerical and categorical covariates ###
+### Case 2 - When need to obtain predictions for a dataframe/matrix containing model inputs including past time series lags and numerical/categorical covariates ###
 
 # Training SETAR-Forest
-forest2 <- setarforest(data = train_df[-1],
-                       label = train_df[,1],
+forest2 <- setarforest(data = web_traffic_train[-1],
+                       label = web_traffic_train[,1],
                        bagging_freq = 3,
                        stopping_criteria = "error_imp",
-                       categorical_covariates = c("Open", "Promo", "StateHoliday", "SchoolHoliday"))
+                       categorical_covariates = c("Project", "Access", "Agent"))
 
 # Obtaining predictions
-forecast(forest2, test_df)
+setartree:::forecast(forest2, web_traffic_test)
 
 
--- demo/tree_demo.R
+++ demo/tree_demo.R
@@ -1,12 +1,17 @@
+if (!('forecast' %in% rownames(installed.packages())))
+  install.packages("forecast")
+
 library(setartree)
+library(forecast) # The 'forecast' package is only required for plotting in line 17 and 18.
+
 
 ### Case 1 - When need to obtain forecasts for a list of time series ###
 
 # Training SETAR-Tree
-tree1 <- setartree(train_series)
+tree1 <- setartree(chaotic_logistic_series)
 
 # Obtaining predictions
-tree_forecasts <- forecast(tree1, train_series)
+tree_forecasts <- setartree:::forecast(tree1, chaotic_logistic_series)
 
 # Plotting
 plot(tree_forecasts[[1]]) # Plotting the 1st time series
@@ -15,16 +20,16 @@ plot(tree_forecasts[[10]]) # Plotting the 10th time series
 
 
 
-### Case 2 - When need to obtain predictions for a dataframe/matrix containing model inputs including past time series lags, numerical and categorical covariates ###
+### Case 2 - When need to obtain predictions for a dataframe/matrix containing model inputs including past time series lags and numerical/categorical covariates ###
 
 # Training SETAR-Tree
-tree2 <- setartree(data = train_df[-1],
-                   label = train_df[,1],
+tree2 <- setartree(data = web_traffic_train[-1],
+                   label = web_traffic_train[,1],
                    stopping_criteria = "error_imp",
-                   categorical_covariates = c("Open", "Promo", "StateHoliday", "SchoolHoliday"))
+                   categorical_covariates = c("Project", "Access", "Agent"))
 
 # Obtaining predictions
-forecast(tree2, test_df)
+setartree:::forecast(tree2, web_traffic_test)
 
 
 
